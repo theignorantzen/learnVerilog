48983bdd54f3a02de946f93dcf97e4b88948519c
theignorantzen <37381724+theignorantzen@users.noreply.github.com>
2020-05-28 08:34:24 +0530

    16-adder module and16 and its tb alutest added

---
+++ add16.v
@@ -0,0 +1,11 @@
+module ALU(X,Y,Z,Sign,Zero,Carry,Parity,Overflow);
+input [15:0]X,Y;
+output [15:0]Z;
+output Sign, Zero, Carry, Parity, Overflow;
+
+assign {Carry,Z} = X + Y;
+assign Sign = Z[15];
+assign Zero = ~|Z;
+assign Parity = ~^Z;
+assign Overflow = (X[15]&Y[15]&~Z[15]) | (~X[15]&~Y[15]&Z[15]);
+endmodule
